1afb3125c6e08e321c7f3b2768fd844f101e2527
eric <ericarterbridge@gmail.com>
2022-07-23 20:50:45 -0400

    worked on student and classroom classes

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,75 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class Classroom {
+
+    Student[] students;
+    int maxNumberOfStudents;
+    List<Student> studentList = new ArrayList<>();
+
+    public Classroom(Student[] students){
+        this.students = students;
+    }
+
+    public Classroom(int maxNumberOfStudents){
+        this.maxNumberOfStudents = maxNumberOfStudents;
+    }
+
+    public Classroom(){
+        students = new Student[30];
+    }
+
+    public Student[] getStudents(){
+        students = studentList.toArray(students);
+        return this.students;
+    }
+
+    public double getAverageExamScore(){
+        int total = 0;
+        double average = 0;
+        for (int i = 0; i<students.length; i++){
+            total += students[i].getAverageExamScore();
+            average = (double)total/students.length;
+        }return average;
+    }
+
+    public void addStudent(Student student){
+
+        studentList.add(student);
+    }
+
+    public void removeStudent (String firstName, String lastName){
+        //Student[] tempArray = new Student[students.length];
+        List<Student> tempList = new ArrayList<>();
+        int arrLength = students.length;
+        for (int i = 0; i<students.length; i++) {
+            if (!Objects.equals(students[i].getFirstName(), firstName) &&
+                    !Objects.equals(students[i].getLastName(), lastName)){
+                tempList.add(students[i]);
+            }
+        }
+        //students = null;
+        students = new Student[arrLength];
+        students = tempList.toArray(students);
+    }
+
+    public Student[] getStudentsByScore(){
+        boolean swapped = true;
+        while (swapped){
+            swapped = false;
+            for (int i = 0; i < students.length-1; i++){
+                if(students[i].getAverageExamScore()<students[i+1].getAverageExamScore()){
+                    Student temp = students[i];
+                    students[i] = students[i+1];
+                    students[i+1] = temp;
+                    swapped = true;
+                }
+            }
+        } return students;
+    }
+
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,66 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    public String firstName;
+    public String lastName;
+    public ArrayList<Double> examScores;
+    public Student(String firstName, String lastName, Double[] testScores){
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+    public String getExamScores() {
+        String examsString = "";
+        for (int i = 0; i < examScores.size(); i++){
+            examsString += "Exam "+(i+1)+" -> "+examScores.get(i)+"\n";
+        }
+        return examsString;
+    }
+
+    public void addExamScore(double examScore){
+        examScores.add(examScore);
+    }
+
+    public void setExamScore(int examNumber, double newScore){
+
+        examScores.set(examNumber-1, newScore);
+
+    }
+
+    public double getAverageExamScore(){
+        int total = 0;
+        double average = 0;
+        for (int i = 0; i<examScores.size(); i++){
+            total += examScores.get(i);
+            average = (double)total/examScores.size();
+        }return average;
+    }
+
+    public String toString(){
+        return "Student Name: " + getFirstName() + getLastName() + "\n" +
+                "> Average Score: " + getAverageExamScore() + "\n" +
+                "> Exam Scores:\n" + getExamScores();
+    }
 }
